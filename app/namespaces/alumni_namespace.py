c5eb789425c5cef86be49de6de60b62aaacd5d16
Viktoriia Kozopas <viktoriiakozopas@Viktoriias-MacBook-Pro.local>
2020-02-16 22:38:28 +0200

    Add offset and limit query params for pagination

--- app/namespaces/alumni_namespace.py
+++ app/namespaces/alumni_namespace.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_restplus import Namespace, Resource
 
 
@@ -5,6 +6,9 @@ api_alumni = Namespace('alumni', description='Requests to alumni model')
 
 
 @api_alumni.route("/")
+@api_alumni.doc(params={
+                    'offset': 'Offset value for pagination. Default: 0.',
+                    'limit': 'Limit value for pagination. Default: 0.'})
 class Alumni(Resource):
 
     def get(self):
@@ -12,12 +16,18 @@ class Alumni(Resource):
         Return all alumni
         """
 
+        query_params = request.args
+        offset = query_params.get('offset', 0)
+        limit = query_params.get('limit', 0)
+
         from app.main import odoo_db, odoo_uid, odoo_password, odoo_models
         contacts = odoo_models.execute_kw(odoo_db, odoo_uid, odoo_password, 'res.partner', 'search_read',
                 [[['is_company', '=', False]]], 
                 {'fields': ['name', 'email', 'function', 'parent_id', 'facebook_link', 'linkedin_link',
                 'bachelor_degree', 'bachelor_faculty', 'bachelor_speciality', 'bachelor_year_in', 'bachelor_year_out',
                 'master_degree', 'master_faculty', 'master_speciality', 'master_year_in', 'master_year_out',
-                'image_1920']})
+                'image_1920'],
+                'offset': int(offset),
+                'limit': int(limit)})
 
         return contacts
